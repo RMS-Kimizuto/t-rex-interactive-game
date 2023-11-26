22ea0d41a12291e4ff30c9216d19a011f858196d
RMS-Kimizuto <lgtan47@gmail.com>
2023-11-26 11:23:19 +0700

    Add kbhit

--- main.cpp
+++ main.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
+#include <conio.h>
+
 using namespace std;
 
-int main()
-{
-    string name;
-    while (true)
-    {
-        getline(cin, name);
-        if (name == "")
-            break;
-        cout << "Hello " << name << endl;
+int main() {
+    char key;
+    int arrowKey;
+    while (true) {
+        
+        if (_kbhit()) {
+            key = _getch();
+            if (key == 0) {
+                arrowKey = _getch();
+                cout << "Arrow key pressed: ";
+                switch (arrowKey) {
+                    case 72:
+                        cout << "UP";
+                        break;
+                    case 80:
+                        cout << "DOWN";
+                        break;
+                    case 75:
+                        cout << "LEFT";
+                        break;
+                    case 77:
+                        cout << "RIGHT";
+                        break;
+                    default:
+                        cout << "Unknown";
+                }
+                cout << endl;
+            } else {
+                cout << "Key pressed: " << key << endl;
+                if (key == 'q') {
+                    break;
+                }
+            }
+        }
     }
-    cout << "Hello Everyone";  
     return 0;
 }
